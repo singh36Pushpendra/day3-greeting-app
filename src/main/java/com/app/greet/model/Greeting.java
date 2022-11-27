cf06f147a92ad4b34ab90a0da60a454e86da80c6
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-27 17:54:41 +0530

    First Commit

---
+++ src/main/java/com/app/greet/model/Greeting.java
@@ -0,0 +1,11 @@
+package com.app.greet.model;
+
+public class Greeting {
+    private Long count;
+    private String name;
+
+    public Greeting(Long count, String name) {
+        this.count = count;
+        this.name = name;
+    }
+}
